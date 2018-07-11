c1c38f48fafc7e32445199d896251954814ffce7
Bolivar <berriobolivar@gmail.com>
2018-07-11 02:04:24 -0500

    get movies filter

--- src/Resources/MovieResource.py
+++ src/Resources/MovieResource.py
@@ -1,20 +1,18 @@
 from Util.BodyParser import BodyParser
 from flask_restful import Resource
 from flask_jwt import jwt_required
+from Util.BodyParser import BodyParser
+from flask import request
 
 from DataAccessLayer.Tmdb import Tmdb
 
 
 class Movie(Resource):
 
-    def get(self, page):
+    def get(self):
         tmdb = Tmdb()
-        results = tmdb.getMovieByPage(page)
-        if results:
-            # genres = tmdb.getGenres()
-            # # for genre in genres['genres']:
-            # #         print(genre['id'])
-            # for result in results['results']:
-            #     print(result)
-            return {'data': results}
-        return {'data': None, 'message': 'There is a error with the data base...'}, 500
+        args = request.args
+        results = tmdb.filterMovies(args['page'], args['id'], args['gender'])
+        if results is None:
+            return {'data': None, 'message': 'There is a error with the data base...'}, 500
+        return {'response': results}
