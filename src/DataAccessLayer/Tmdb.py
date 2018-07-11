27eb571c101d5795f342cf7e93a2211200f6b427
Bolivar <berriobolivar@gmail.com>
2018-07-11 02:04:08 -0500

    added filter method

--- src/DataAccessLayer/Tmdb.py
+++ src/DataAccessLayer/Tmdb.py
@@ -1,17 +1,22 @@
 import tmdbsimple as tmdb
 import app as app
 
-class Tmdb():
+class Tmdb(object):
 
     def __init__(self):
         tmdb.API_KEY = app.app.config.get('TMDB_API_KEY')
 
-    def getMovieByPage(self, page):
-        movies = tmdb.Movies()
-        response = movies.now_playing(page=page)
-        return response
-
-    def getGenres(self):
-        genres = tmdb.Genres()
-        response = genres.movie_list()
-        return response
+    def filterMovies(self, page, id, gender):
+        if(page):
+            response = tmdb.Movies()
+            response = response.now_playing(page=page)
+            return response
+        if(id):
+            movieInfo = tmdb.Movies(id).info()
+            return movieInfo
+        if(gender):
+            genres = tmdb.Genres()
+            response = genres.movie_list()
+            return response
+        
+        return None
